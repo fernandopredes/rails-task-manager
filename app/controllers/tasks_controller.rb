17f7d416424db46665159dcacc0bfc65eb8243de
Fernando Predes <fernando_predes@hotmail.com>
2022-05-24 16:20:41 -0300

    solução

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,19 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'tasks', to: 'tasks#index'
+  get 'index', to: 'tasks#index'
+
+  get 'index/new', to: 'tasks#new', as: :new
+  post 'tasks', to: 'tasks#create'
+
   get 'index/:id', to: 'tasks#show', as: :task
 end
