a7929ea14ab5af68194ccddd91307f9b7099aac7
Fernando Predes <fernando_predes@hotmail.com>
2022-05-24 17:10:14 -0300

    created all CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'index', to: 'tasks#index'
-
+  get 'index', to: 'tasks#index', as: :tasks
   get 'index/new', to: 'tasks#new', as: :new
-  post 'tasks', to: 'tasks#create'
-
+  post 'index', to: 'tasks#create'
+  get 'index/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'index/:id', to: 'tasks#update'
   get 'index/:id', to: 'tasks#show', as: :task
+  delete 'index/:id', to: 'tasks#destroy', as: :delete
 end
