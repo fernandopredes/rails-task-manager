d6ba00feb46ef14f90dd176aa65af176ae2aa154
Fernando Predes <fernando_predes@hotmail.com>
2022-05-24 15:30:18 -0300

    created a link to show the details

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index'
+  get 'index/:id', to: 'tasks#show', as: :task
 end
